94ee73d2e1fd93af990e85652785c96c5f316215
Joe Clarke <joeman113@gmail.com>
2022-09-29 18:24:43 -0400

    donezo

--- powershell/azure_vm_HANA_clone.ps1
+++ powershell/azure_vm_HANA_clone.ps1
@@ -1,7 +1,6 @@
 #Clone 1 HANA VM from azure-159.97.56.0-23-prod to azure-159.97.56.0-23-prod in the WR Grace Azure subscription
 
 #New VM Name; same network. Same OS Disk. 
-#Assumes Linux virtual machine
 
 #******************************************
 # Virtual Machine: Source Variables
@@ -36,7 +35,7 @@ $snapshotNameData3 = $sourceVirtualMachineName + '_DataDisk-3-snapshot'
 #$virtualNetworkName = 'North-Central-Production'
 $virtualNetworkName = 'bubble-sles-tester-vnet'
 #$subnetName = 'azure-159.97.56.0-23-prod'
-$subnetName = 'azure-159.97.56.0-23-prod'
+$subnetName = 'bubble-azure-159.97.56.0-23-prod'
 #$resourceGroupForDestinationVNet = 'RG-PROD-MGMT'
 $resourceGroupForDestinationVNet = 'bubble-sles-tester'
 #$location = 'northcentralus'
@@ -52,16 +51,20 @@ $targetVirtualMachineIP = '159.97.56.16'
 # ^ not needed
 
 #Name of the destination Managed Disks
-$diskNameOS = $targetVirtualMachineName + '_OsDisk'  
-$diskNameData0 = $targetVirtualMachineName + '_DataDisk0'  
+$diskNameOS = $targetVirtualMachineName + '_OsDisk'
+$diskNameData0 = $targetVirtualMachineName + '_DataDisk0'
 $diskNameData1 = $targetVirtualMachineName + '_DataDisk1'
+
+#Operating System Type (Windows/Linux)  
+$targetOStype = "Linux"
   
 #Size of destination Managed Disk(s) in GB  
 #User 0 size for disk if there is no other disk
-$diskSize = 30
+#$diskSize = 30
 #Now deterministaclly found
   
-#Storage type for the new Managed Disk (Standard_LRS / Premium_LRS / StandardSSD_LRS)  
+#Storage type for the desired cloned Managed Disk (Available values are Standard_LRS, Premium_LRS, StandardSSD_LRS, and UltraSSD_LRS)
+#https://learn.microsoft.com/en-us/powershell/module/azurerm.compute/new-azurermdiskconfig?view=azurermps-6.13.0#-skuname
 $storageTypeOS = 'Standard_LRS'
 $storageTypeData0 = 'Standard_LRS'
 $storageTypeData1 = 'Standard_LRS'
@@ -95,32 +98,41 @@ If (!$newRgAlreadyExists) {New-AzResourceGroup -Name $resourceGroupNameCloned -L
 
 Write-Host "Taking snapshots of disks..."
   
-#Create new OS VM Disk Snapshot in source environment 
+#Create new OS VM Disk Snapshot from source VM 
 $diskSizeOS = $sourceVirtualMachine.StorageProfile.OsDisk.diskSizeGB
-$snapshotOSconfig = New-AzSnapshotConfig -SourceUri $sourceVirtualMachine.StorageProfile.OsDisk.ManagedDisk.Id -Location $location -CreateOption copy -DiskSizeGB $diskSize -AccountType Standard_LRS
+$snapshotOSconfig = New-AzSnapshotConfig -SourceUri $sourceVirtualMachine.StorageProfile.OsDisk.ManagedDisk.Id -Location $location -CreateOption copy -DiskSizeGB $diskSizeOS -AccountType $storageTypeOS
 $snapshotOS = New-AzSnapshot -Snapshot $snapshotOSconfig -SnapshotName $snapshotNameOS -ResourceGroupName $resourceGroupName
 
-#Create new Data Disk 0 Snapshot in source environment 
+#Create new Data Disk 0 Snapshot from source VM 
 $diskSizeData0 = $sourceVirtualMachine.StorageProfile.dataDisks.diskSizeGB[0]
-$snapshotData0 = New-AzSnapshotConfig -SourceUri $sourceVirtualMachine.StorageProfile.DataDisks.ManagedDisk.Id[0] -Location $location -CreateOption copy -DiskSizeGB $diskSizeData0
-$snapshotData0 = New-AzSnapshot -Snapshot $snapshotData0 -SnapshotName $snapshotNameData0 -ResourceGroupName $resourceGroupName
+$snapshotData0config = New-AzSnapshotConfig -SourceUri $sourceVirtualMachine.StorageProfile.DataDisks.ManagedDisk.Id[0] -Location $location -CreateOption copy -DiskSizeGB $diskSizeData0
+$snapshotData0 = New-AzSnapshot -Snapshot $snapshotData0config -SnapshotName $snapshotNameData0 -ResourceGroupName $resourceGroupName
 
-#Create new Data Disk 1 Snapshot in source environment
+#Create new Data Disk 1 Snapshot from source VM 
 $diskSizeData1 = $sourceVirtualMachine.StorageProfile.dataDisks.diskSizeGB[1]
-$snapshotData1 = New-AzSnapshotConfig -SourceUri $sourceVirtualMachine.StorageProfile.DataDisks.ManagedDisk.Id[1] -Location $location -CreateOption copy -DiskSizeGB $diskSizeData1
-$snapshotData1 = New-AzSnapshot -Snapshot $snapshotData1 -SnapshotName $snapshotNameData1 -ResourceGroupName $resourceGroupName
+$snapshotData1config = New-AzSnapshotConfig -SourceUri $sourceVirtualMachine.StorageProfile.DataDisks.ManagedDisk.Id[1] -Location $location -CreateOption copy -DiskSizeGB $diskSizeData1
+$snapshotData1 = New-AzSnapshot -Snapshot $snapshotData1config -SnapshotName $snapshotNameData1 -ResourceGroupName $resourceGroupName
 
 Write-Host "Creating managed disks..."
-
 #Create a new OS Managed Disk from the Snapshot
-$diskOS = New-AzureRmDiskConfig -AccountType $storageType -DiskSizeGB $diskSize -Location $location -CreateOption Copy -SourceResourceId $snapshotOS.Id
+$diskOS = New-AzureRmDiskConfig -AccountType $storageTypeOS -DiskSizeGB $diskSizeOS -Location $location -CreateOption Copy -SourceResourceId $snapshotOS.Id
 $diskOS = New-AzureRmDisk -Disk $diskOS -ResourceGroupName $resourceGroupNameCloned -DiskName $diskNameOS
+
+#Create a new Data Disk 0 from the Snapshot
+$diskData0 = New-AzureRmDiskConfig -AccountType $storageTypeData0 -DiskSizeGB $diskSizeData0 -Location $location -CreateOption Copy -SourceResourceId $snapshotData0.Id
+$diskData0 = New-AzureRmDisk -Disk $diskData0 -ResourceGroupName $resourceGroupNameCloned -DiskName $diskNameData0
+
+#Create a new Data Disk 1 from the Snapshot
+$diskData1 = New-AzureRmDiskConfig -AccountType $storageTypeData1 -DiskSizeGB $diskSizeData1 -Location $location -CreateOption Copy -SourceResourceId $snapshotData1.Id
+$diskData1 = New-AzureRmDisk -Disk $diskData1 -ResourceGroupName $resourceGroupNameCloned -DiskName $diskNameData1
 
 #Initialize virtual machine configuration  
 $targetVirtualMachine = New-AzureRmVMConfig -VMName $targetVirtualMachineName -VMSize $targetVirtualMachineSize
   
-#Attach Managed Disk to target virtual machine. OS type depends OS present in the disk (Windows/Linux)  
-$targetVirtualMachine = Set-AzureRmVMOSDisk -VM $targetVirtualMachine -ManagedDiskId $diskOS.Id -CreateOption Attach -Linux  
+#Attach Managed Disks to target virtual machine. OS type depends variable set in destination variable section (Windows/Linux)  
+$targetVirtualMachine = Set-AzureRmVMOSDisk -VM $targetVirtualMachine -ManagedDiskId $diskOS.Id -CreateOption Attach Linux
+$targetVirtualMachine = Add-AzVMDataDisk -VM $targetVirtualMachine -Name $diskNameData0 -CreateOption Attach -ManagedDiskId $diskData0.Id -Lun 0
+$targetVirtualMachine = Add-AzVMDataDisk -VM $targetVirtualMachine -Name $diskNameData1 -CreateOption Attach -ManagedDiskId $diskData1.Id -Lun 1
   
 #Set destination VNet Variable Destination Virtual Network information  
 $vnet = Get-AzureRmVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $resourceGroupForDestinationVNet
