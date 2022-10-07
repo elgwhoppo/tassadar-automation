75295baad0503eff1fc126e760e56cdba5276705
Joe Clarke <joeman113@gmail.com>
2022-10-06 22:30:14 -0400

    vm no longer needs powered on

--- powershell/azure_vm_HANA_clone.ps1
+++ powershell/azure_vm_HANA_clone.ps1
@@ -1,6 +1,10 @@
-#Clone 1 HANA VM from azure-159.97.56.0-23-prod to azure-159.97.56.0-23-prod in the WR Grace Azure subscription
+#This script will clone a VM from a source RG and VNet to a destination RG and VNet. 
 
 #New VM Name; same network. Same OS Disk. 
+
+#Note!! The 
+
+#Todo: Check if snapshots exist with the same name 
 
 #******************************************
 # Virtual Machine: Source Variables
@@ -21,11 +25,9 @@ $resourceGroupName = 'sles-tester'
 $sourceVirtualMachineName = 'sles-tester'
 
 #Name of snapshot which will be created from the Managed Disk
-$snapshotNameOS = $sourceVirtualMachineName + '_OsDisk-snapshot'
-$snapshotNameData0 = $sourceVirtualMachineName + '_DataDisk-0-snapshot'
-$snapshotNameData1 = $sourceVirtualMachineName + '_DataDisk-1-snapshot'
-$snapshotNameData2 = $sourceVirtualMachineName + '_DataDisk-2-snapshot'
-$snapshotNameData3 = $sourceVirtualMachineName + '_DataDisk-3-snapshot'
+$snapshotNameOS = $sourceVirtualMachineName + '_OsDisk-snapshot-cloning-powershell'
+$snapshotNameData0 = $sourceVirtualMachineName + '_DataDisk-0-snapshot-cloning-powershell'
+$snapshotNameData1 = $sourceVirtualMachineName + '_DataDisk-1-snapshot-cloning-powershell'
 
 #**********************************************
 # Virtual Machine: Destination Variables
@@ -55,16 +57,18 @@ $diskNameOS = $targetVirtualMachineName + '_OsDisk'
 $diskNameData0 = $targetVirtualMachineName + '_DataDisk0'
 $diskNameData1 = $targetVirtualMachineName + '_DataDisk1'
 
-#Operating System Type (Windows/Linux)  
-#$targetOStype = "-Linux"
+#Operating System Type (-Windows/-Linux)  
+$targetOStype = "-Linux"
   
 #Storage type for the desired cloned Managed Disk (Available values are Standard_LRS, Premium_LRS, StandardSSD_LRS, and UltraSSD_LRS)
 #https://learn.microsoft.com/en-us/powershell/module/azurerm.compute/new-azurermdiskconfig?view=azurermps-6.13.0#-skuname
 $storageTypeOS = 'Standard_LRS'
-$storageTypeData0 = 'Standard_LRS'
-$storageTypeData1 = 'Standard_LRS'
+$storageTypeData0 = 'Premium_LRS'
+$storageTypeData1 = 'Premium_LRS'
   
 #Size of the Virtual Machine (https://docs.microsoft.com/en-us/azure/virtual-machines/windows/sizes)  
+# to get powershell friendly names of the sizes for each region:   az vm list-sizes --location "eastus" --output table
+# to get powershell friendly names of the regions:                 Get-AzLocation | select displayname,location
 $targetVirtualMachineSize = 'Standard_B2ms'
   
 #*********************************
@@ -87,18 +91,32 @@ $newRgAlreadyExists = Get-AzureRmResourceGroup -Name $resourceGroupNameCloned
 If (!$newRgAlreadyExists) {New-AzResourceGroup -Name $resourceGroupNameCloned -Location $location} else {Write-Host "Resource group exists already. Skipping creation"}
 
 Write-Host "Taking snapshots of disks..."
+#look for any pre-existing snapshots with the exact name. Delete them. 
+$osSnapShotExists = az snapshot show --resource-group $resourceGroupNameCloned --name $snapshotNameOS
+If ($osSnapShotExists) {Remove-AzureRmSnapshot -ResourceGroupName $resourceGroupNameCloned -SnapshotName $snapshotNameOS -Force} else {Write-Host "No snapshots found with the name $snapshotNameOS, continuing with creating fresh snapshots."}
 #Create new OS VM Disk Snapshot from source VM
-$diskSizeOS = $sourceVirtualMachine.StorageProfile.OsDisk.diskSizeGB
-$snapshotOSconfig = New-AzSnapshotConfig -SourceUri $sourceVirtualMachine.StorageProfile.OsDisk.ManagedDisk.Id -Location $location -CreateOption copy -DiskSizeGB $diskSizeOS -AccountType $storageTypeOS
+#Lesson learned, if you use $sourceVirtualMachine.StorageProfile.OsDisk.diskSizeGB it only works if the VM is powered on. This way it works both. 
+$diskNameOS = $sourceVirtualMachine.StorageProfile.OsDisk.Name
+$diskSizeOS = az disk show --name $diskNameOS --resource-group $resourceGroupName --query diskSizeGb
+$snapshotOSconfig = New-AzSnapshotConfig -SourceUri $sourceVirtualMachine.StorageProfile.OsDisk.ManagedDisk.Id -Location $location -CreateOption copy -DiskSizeGB $diskSizeOS
 $snapshotOS = New-AzSnapshot -Snapshot $snapshotOSconfig -SnapshotName $snapshotNameOS -ResourceGroupName $resourceGroupNameCloned
 
-#Create new Data Disk 0 Snapshot from source VM 
-$diskSizeData0 = $sourceVirtualMachine.StorageProfile.dataDisks.diskSizeGB[0]
+
+#Create new Data Disk 0 Snapshot from source VM
+#look for any pre-existing snapshots with the exact name. Delete them. 
+$data0SnapShotExists = az snapshot show --resource-group $resourceGroupNameCloned --name $snapshotNameData0
+If ($data0SnapShotExists) {Remove-AzureRmSnapshot -ResourceGroupName $resourceGroupNameCloned -SnapshotName $snapshotNameData0 -Force} else {Write-Host "No snapshots found with the name $snapshotNameData0, continuing with creating fresh snapshots."}
+$diskNameData0 = $sourceVirtualMachine.StorageProfile.dataDisks.Name[0]
+$diskSizeData0 = az disk show --name $diskNameData0 --resource-group $resourceGroupName --query diskSizeGb
 $snapshotData0config = New-AzSnapshotConfig -SourceUri $sourceVirtualMachine.StorageProfile.DataDisks.ManagedDisk.Id[0] -Location $location -CreateOption copy -DiskSizeGB $diskSizeData0
 $snapshotData0 = New-AzSnapshot -Snapshot $snapshotData0config -SnapshotName $snapshotNameData0 -ResourceGroupName $resourceGroupNameCloned
 
 #Create new Data Disk 1 Snapshot from source VM 
-$diskSizeData1 = $sourceVirtualMachine.StorageProfile.dataDisks.diskSizeGB[1]
+#look for any pre-existing snapshots with the exact name. Delete them. 
+$data1SnapShotExists = az snapshot show --resource-group $resourceGroupNameCloned --name $snapshotNameData1
+If ($data1SnapShotExists) {Remove-AzureRmSnapshot -ResourceGroupName $resourceGroupNameCloned -SnapshotName $snapshotNameData1 -Force} else {Write-Host "No snapshots found with the name $snapshotNameData1, continuing with creating fresh snapshots."}
+$diskNameData1 = $sourceVirtualMachine.StorageProfile.dataDisks.Name[1]
+$diskSizeData1 = az disk show --name $diskNameData1 --resource-group $resourceGroupName --query diskSizeGb
 $snapshotData1config = New-AzSnapshotConfig -SourceUri $sourceVirtualMachine.StorageProfile.DataDisks.ManagedDisk.Id[1] -Location $location -CreateOption copy -DiskSizeGB $diskSizeData1
 $snapshotData1 = New-AzSnapshot -Snapshot $snapshotData1config -SnapshotName $snapshotNameData1 -ResourceGroupName $resourceGroupNameCloned
 
@@ -129,7 +147,7 @@ $vnet = Get-AzureRmVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $
 $subnet = $vnet.Subnets | Where-Object {$_.Name -eq $subnetName}
 
 # Create Network Interface for the VM without Public IP, and with the IP placed in the variables above 
-$nic = New-AzureRmNetworkInterface -Name ($targetVirtualMachineName.ToLower() + '_nic') -ResourceGroupName $resourceGroupNameCloned -Location $location -SubnetId $subnet.Id -PrivateIpAddress $targetVirtualMachineIP
+$nic = New-AzureRmNetworkInterface -Name ($targetVirtualMachineName.ToLower() + '_nic') -ResourceGroupName $resourceGroupNameCloned -Location $location -SubnetId $subnet.Id -PrivateIpAddress $targetVirtualMachineIP -Force
 $targetVirtualMachine = Add-AzureRmVMNetworkInterface -VM $targetVirtualMachine -Id $nic.Id
 
 Write-Host "Create VM..."
